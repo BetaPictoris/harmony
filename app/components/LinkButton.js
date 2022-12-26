15d9060d98b60a9b4e74a17aea1f56eb4417cfab
BetaPictoris <beta@ozx.me>
2022-12-25 18:39:20 -0800

    Added `LinkButton` styles

--- app/components/LinkButton.js
+++ app/components/LinkButton.js
@@ -1,12 +1,19 @@
+import "./styles/LinkButton.scss";
+
 export default function LinkButton(props) {
+  var activeClass = "unactive";
+  if (props.active) {
+    activeClass = "active";
+  }
+
   return (
-    <a className="LinkButton" href={`/app#${props.path}`}>
+    <a className={`LinkButton ${activeClass}`} href={`/app#${props.path}`}>
       <img
         width={32}
         src={`/app/assets/svg/icons/${props.icon}.svg`}
         alt={props.alt ? props.alt : `${props.text}`}
       />
-      {props.text}
+      <span className="LinkText">{props.text}</span>
     </a>
   );
 }
