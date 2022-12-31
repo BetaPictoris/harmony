90356537b33f5fe7a0fd02aba71237abab37a66d
BetaPictoris <beta@ozx.me>
2022-12-31 02:08:32 -0800

    Fixed styles

--- app/components/Sidebar.js
+++ app/components/Sidebar.js
@@ -17,17 +17,24 @@ export default function Sidebar(props) {
   return (
     <div className="sidebar">
       <div className="sidebarHeader">
-        <img className="headerImg" src="/assets/svg/Ico.svg" alt="Harmony" />
+        <img
+          className="headerImg"
+          src="/app/assets/svg/Ico.svg"
+          alt="Harmony"
+          width="64px"
+        />
       </div>
 
       <div className="sidebarLinks">
         {pages.map((page) => (
-          <LinkButton
-            path={page === "home" ? "" : page}
-            icon={page}
-            text={page.charAt(0).toUpperCase() + page.slice(1)}
-            active={isPage(page, p)}
-          />
+          <span className="sidebarLink">
+            <LinkButton
+              path={page === "home" ? "" : page}
+              icon={page}
+              text={page.charAt(0).toUpperCase() + page.slice(1)}
+              active={isPage(page, p)}
+            />
+          </span>
         ))}
       </div>
     </div>
