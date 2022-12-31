9dd0adbbe0bafaba4c673258bfca29600761d696
BetaPictoris <beta@ozx.me>
2022-12-31 14:55:16 -0800

    Fixed page size

--- app/pages/HomePage.js
+++ app/pages/HomePage.js
@@ -1,7 +1,20 @@
+import Section from "../components/Section";
+
 export default function HomePage() {
   return (
     <div className="home page">
       <h1 className="pageTitle">Welcome back!</h1>
+      <div className="homeContent pageContent">
+        <Section title="Jump back in...">
+          Recently play albums placeholder
+        </Section>
+      </div>
+      <div className="homeFooter pageFooter">
+        Made with <span className="heart">❤️</span> by{" "}
+        <a href="//github.com/BetaPictoris" target="_blank">
+          Beta Pictoris
+        </a>
+      </div>
     </div>
   );
 }
