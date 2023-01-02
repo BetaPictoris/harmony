6b0c453745a685079ff1890399dd130a3c8e3975
BetaPictoris <beta@ozx.me>
2023-01-01 20:41:11 -0800

    Added album styles

--- app/components/Album.js
+++ app/components/Album.js
@@ -3,7 +3,7 @@ import "./styles/Album.scss";
 export default function Album(props) {
   return (
     <div className="album">
-      <a href={`#app/album/${props.id}`}>
+      <a className="albumLink" href={`#app/album/${props.id}`}>
         <img
           class="albumImg"
           src={`/api/v1/albums/${props.id}/cover`}
