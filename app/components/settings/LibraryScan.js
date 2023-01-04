1e7298250945c32ec2999b03b630d020997035c2
BetaPictoris <beta@ozx.me>
2023-01-03 22:20:42 -0800

    Moved `Button` comp file

--- app/components/settings/LibraryScan.js
+++ app/components/settings/LibraryScan.js
@@ -1,5 +1,5 @@
 import React from "react";
-import Button from "./Button";
+import Button from "../Button";
 
 export default function LibraryScan(props) {
   const [checkScanning, setCheckScanning] = React.useState(false); // If true check `isScanning` every 1 second
