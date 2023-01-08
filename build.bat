3a2b48895b248eee1e5c9cc669ccf666784f0ad2
BetaPictoris <beta@ozx.me>
2023-01-07 16:53:47 -0800

    Made `build.bat` easier to use

--- build.bat
+++ build.bat
@@ -1,10 +1,16 @@
 @echo off
 
-mkdir build
+del .\build\harmony.exe
+del .\build\app 
+
+mkdir .\build\
 
 cd api
 go build -o ../build
 move ..\build\api.exe ..\build\harmony.exe
 cd ..
+
+mkdir .\build\data\
+copy .\examples\config.ini .\build\data\config.ini
 
 npm run build
